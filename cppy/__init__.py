d90063d383bfe2c435838f94ecc0d6dc0700bed3
Chris Colbert <sccolbert@gmail.com>
2014-09-27 16:14:49 -0400

    update py version

--- cppy/__init__.py
+++ cppy/__init__.py
@@ -5,11 +5,17 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-import os
+__major_version__ = 1
+__minor_version__ = 0
+__patch_version__ = 0
 
 
-__version__ = 3  # kept in sync with CPPY_VERSION
+__version_info__ = (__major_version__, __minor_version__, __patch_version__)
+
+
+__version__ = '%s.%s.%s' % __version_info__
 
 
 def get_include():
+    import os
     return os.path.join(os.path.dirname(__file__), 'include')
