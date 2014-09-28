c0ee17689bef78ce55e45d60afabbc61a67c433f
S. Chris Colbert <sccolbert@gmail.com>
2014-09-28 14:14:19 -0400

    spaces -> tabs

--- cppy/include/cppy/errors.h
+++ cppy/include/cppy/errors.h
@@ -15,57 +15,58 @@ namespace cppy
 
 inline PyObject* system_error( const char* message )
 {
-    PyErr_SetString( PyExc_SystemError, message );
-    return 0;
+	PyErr_SetString( PyExc_SystemError, message );
+	return 0;
 }
 
 
 inline PyObject* type_error( const char* message )
 {
-    PyErr_SetString( PyExc_TypeError, message );
-    return 0;
+	PyErr_SetString( PyExc_TypeError, message );
+	return 0;
 }
 
 
 inline PyObject* type_error( PyObject* ob, const char* expected )
 {
-    PyErr_Format(
-        PyExc_TypeError,
-        "Expected object of type `%s`. Got object of type `%s` instead.",
-        expected,
-        ob->ob_type->tp_name );
-    return 0;
+	PyErr_Format(
+		PyExc_TypeError,
+		"Expected object of type `%s`. Got object of type `%s` instead.",
+		expected,
+		ob->ob_type->tp_name );
+	return 0;
 }
 
 
 inline PyObject* value_error( const char* message )
 {
-    PyErr_SetString( PyExc_ValueError, message );
-    return 0;
+	PyErr_SetString( PyExc_ValueError, message );
+	return 0;
 }
 
 
 inline PyObject* runtime_error( const char* message )
 {
-    PyErr_SetString( PyExc_RuntimeError, message );
-    return 0;
+	PyErr_SetString( PyExc_RuntimeError, message );
+	return 0;
 }
 
 
 inline PyObject* attribute_error( const char* message )
 {
-    PyErr_SetString( PyExc_AttributeError, message );
-    return 0;
+	PyErr_SetString( PyExc_AttributeError, message );
+	return 0;
 }
 
 
 inline PyObject* attribute_error( PyObject* ob, const char* attr )
 {
-    PyErr_Format( PyExc_AttributeError,
-                  "'%s' object has no attribute '%s'",
-                  ob->ob_type->tp_name,
-                  attr );
-    return 0;
+	PyErr_Format(
+		PyExc_AttributeError,
+		"'%s' object has no attribute '%s'",
+		ob->ob_type->tp_name,
+		attr );
+	return 0;
 }
 
 } // namespace cppy
