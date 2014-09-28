b75fdf3fff37872fe54cda59b762203dd0bd2f82
S. Chris Colbert <sccolbert@gmail.com>
2014-09-28 16:50:08 -0400

    bump the patch version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='cppy',
-    version='1.0.1',
+    version='1.0.2',
     author='The Nucleic Development Team',
     author_email='sccolbert@gmail.com',
     url='https://github.com/nucleic/cppy',
